b1cef4b5c8bb03eb0da288177da3f4e38756f61b
Markus Hofmann <m.hofmann@web-vision.de>
2023-03-16 15:22:58 +0100

    [TASK] change types to underscored

--- Classes/Domain/Enumeration/Category.php
+++ Classes/Domain/Enumeration/Category.php
@@ -8,17 +8,17 @@ use TYPO3\CMS\Core\Type\Enumeration;
 
 class Category extends Enumeration
 {
-    public const TYPE_APPLICATION_PERIOD = 'applicationPeriod';
+    public const TYPE_APPLICATION_PERIOD = 'application_period';
 
-    public const TYPE_BEGIN_COURSE = 'beginCourse';
+    public const TYPE_BEGIN_COURSE = 'begin_course';
 
     public const TYPE_COSTS = 'costs';
 
     public const TYPE_DEGREE = 'degree';
 
-    public const TYPE_STANDARD_PERIOD = 'standardPeriod';
+    public const TYPE_STANDARD_PERIOD = 'standard_period';
 
-    public const TYPE_COURSE_TYPE = 'courseType';
+    public const TYPE_COURSE_TYPE = 'course_type';
 
-    public const TYPE_TEACHING_LANGUAGE = 'teachingLanguage';
+    public const TYPE_TEACHING_LANGUAGE = 'teaching_language';
 }
