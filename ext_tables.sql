3bb65cd06648b42901f07cd716b0bae3b502e78a
Patryk Kudlik <patryk@web-vision.de>
2023-03-20 11:52:09 +0000

    [TASK] Add TeaserBox Video content element

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,9 +1,9 @@
 CREATE TABLE sys_category (
-  type varchar(40) default '' not null
+	type varchar(40) default '' not null
 );
 
 CREATE TABLE pages (
-  job_profile text,
-  performance_scope text,
-  prerequisites text
+	job_profile text,
+	performance_scope text,
+	prerequisites text
 );
