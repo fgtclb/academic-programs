507542411d95aa9b33f9d70f2fc62b5e7b8d104d
Markus Hofmann <m.hofmann@web-vision.de>
2023-04-03 09:48:21 +0200

    [TASK] Register new study types in enum

--- Classes/Domain/Enumeration/Category.php
+++ Classes/Domain/Enumeration/Category.php
@@ -16,9 +16,13 @@ class Category extends Enumeration
 
     public const TYPE_DEGREE = 'degree';
 
+    public const TYPE_DEPARTMENT = 'department';
+
     public const TYPE_STANDARD_PERIOD = 'standard_period';
 
     public const TYPE_COURSE_TYPE = 'course_type';
 
     public const TYPE_TEACHING_LANGUAGE = 'teaching_language';
+
+    public const TYPE_TOPIC = 'topic';
 }
