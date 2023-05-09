5dbb28dd5e9d401e999e355a183d54bd17eb9c11
Markus Hofmann <m.hofmann@web-vision.de>
2023-05-09 12:41:48 +0200

    [TASK] Introduce category settings in plugin

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -8,4 +8,12 @@ declare(strict_types=1);
         'CourseList',
         'Educational Course'
     );
+
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['educationalcourse_courselist'] = 'layout,recursive';
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['educationalcourse_courselist'] = 'pi_flexform';
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+        'educationalcourse_courselist',
+        'FILE:EXT:educational_course/Configuration/FlexForms/CourseSettings.xml'
+    );
 })();
