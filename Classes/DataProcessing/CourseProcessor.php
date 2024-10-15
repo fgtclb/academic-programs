7e36a5ba95fa8ff36cfddd354ce480ecf38c0c7b
Philipp Halle <p.halle@web-vision.de>
2024-10-15 15:05:00 +0200

    [TASK] Generate course object for template output

---
+++ Classes/DataProcessing/CourseProcessor.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace FGTCLB\EducationalCourse\DataProcessing;
+
+use FGTCLB\EducationalCourse\Factory\CourseFactory;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+use TYPO3\CMS\Frontend\ContentObject\DataProcessorInterface;
+
+/**
+ * Processor class for course page types
+ */
+class CourseProcessor implements DataProcessorInterface
+{
+    /**
+     * Make course data accessable in Fluid
+     *
+     * @param ContentObjectRenderer $cObj The data of the content element or page
+     * @param array<string, mixed> $contentObjectConfiguration The configuration of Content Object
+     * @param array<string, mixed> $processorConfiguration The configuration of this processor
+     * @param array<string, mixed> $processedData Key/value store of processed data (e.g. to be passed to a Fluid View)
+     * @return array<string, mixed> the processed data as key/value store
+     */
+    public function process(
+        ContentObjectRenderer $cObj,
+        array $contentObjectConfiguration,
+        array $processorConfiguration,
+        array $processedData
+    ) {
+        $courseFactory = GeneralUtility::makeInstance(CourseFactory::class);
+        $processedData['course'] = $courseFactory->get($processedData['data']);
+        return $processedData;
+    }
+}
--- Classes/Domain/Model/Course.php
+++ Classes/Domain/Model/Course.php
@@ -4,204 +4,86 @@ declare(strict_types=1);
 
 namespace FGTCLB\EducationalCourse\Domain\Model;
 
-use Doctrine\DBAL\DBALException;
-use Doctrine\DBAL\Driver\Exception;
 use FGTCLB\EducationalCourse\Collection\CategoryCollection;
-use FGTCLB\EducationalCourse\Collection\FileReferenceCollection;
 use FGTCLB\EducationalCourse\Domain\Repository\CategoryRepository;
-use FGTCLB\EducationalCourse\Enumeration\PageTypes;
-use FGTCLB\EducationalCourse\Exception\Domain\CategoryExistException;
-use InvalidArgumentException;
-use TYPO3\CMS\Core\Domain\Repository\PageRepository;
-use TYPO3\CMS\Core\Resource\Exception\FileDoesNotExistException;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Domain\Model\FileReference;
+use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
 
-class Course
+class Course extends AbstractEntity
 {
-    protected int $uid;
+    protected int $doktype;
 
-    protected string $title;
+    protected string $title = '';
 
-    protected string $subtitle;
+    protected string $subtitle = '';
 
-    protected string $abstract;
+    protected string $abstract = '';
 
-    protected string $jobProfile;
+    protected string $jobProfile = '';
 
-    protected string $performanceScope;
+    protected string $performanceScope = '';
 
-    protected string $prerequisites;
+    protected string $prerequisites = '';
 
-    protected CategoryCollection $categories;
+    protected ?CategoryCollection $attributes = null;
 
-    protected FileReferenceCollection $media;
+    /** @var ObjectStorage<FileReference> */
+    protected $media;
 
-    /**
-     * @throws CategoryExistException
-     * @throws Exception
-     * @throws DBALException
-     * @throws FileDoesNotExistException
-     * @throws InvalidArgumentException
-     */
-    public function __construct(int $databaseId)
+    public function getPid(): int
     {
-        /** @var PageRepository $pageRepository */
-        $pageRepository = GeneralUtility::makeInstance(PageRepository::class);
-        $page = $pageRepository->getPage($databaseId);
-        if (count($page) === 0) {
-            throw new InvalidArgumentException(
-                'Page not found',
-                1683811343841
-            );
-        }
-        $this->uid = $page['uid'];
-        $this->title = $page['title'] ?? '';
-        $this->subtitle = $page['subtitle'] ?? '';
-        $this->abstract = $page['abstract'] ?? '';
-        $this->jobProfile = $page['job_profile'] ?? '';
-        $this->performanceScope = $page['performance_scope'] ?? '';
-        $this->prerequisites = $page['prerequisites'] ?? '';
-
-        /** @var CategoryRepository $categoryRepository */
-        $categoryRepository = GeneralUtility::makeInstance(CategoryRepository::class);
-        $this->categories = $categoryRepository ->findAllByPageId($databaseId);
-
-        $this->media = self::loadMedia($this->uid);
+        return $this->pid;
     }
 
-    /**
-     * @throws Exception
-     * @throws DBALException
-     * @throws CategoryExistException
-     * @throws FileDoesNotExistException
-     */
-    public static function loadFromLink(int $linkId): Course
+    public function getUid(): int
     {
-        /** @var PageRepository $pageRepository */
-        $pageRepository = GeneralUtility::makeInstance(PageRepository::class);
-        $pageToResolve = $pageRepository->getPage($linkId);
-        $originalPage = match ($pageToResolve['doktype']) {
-            PageRepository::DOKTYPE_SHORTCUT => $pageRepository->resolveShortcutPage($pageToResolve),
-            // @todo Allow Mountpoints
-            //PageRepository::DOKTYPE_MOUNTPOINT => $pageRepository->getMountPointInfo($linkId, $pageToResolve),
-            default => throw new InvalidArgumentException(
-                'Calling with doktypes other than 4 or 7 not allowed',
-                1685532706120
-            ),
-        };
-        if ($originalPage['doktype'] !== PageTypes::TYPE_EDUCATIONAL_COURSE) {
-            throw new \RuntimeException(
-                sprintf('Page "%d" has no Course page linked', $linkId),
-                1685532982084
-            );
-        }
-
-        $studyPage = new self($originalPage['uid']);
-        if ($pageToResolve['shortcut_overwrite'] === 1) {
-            $studyPage->overwriteFields($pageToResolve);
-        }
-
-        return $studyPage;
+        return $this->uid;
     }
 
-    /**
-     * @throws FileDoesNotExistException
-     * @throws Exception
-     */
-    protected static function loadMedia(int $pageId): FileReferenceCollection
+    public function getDoktype(): int
     {
-        /** @var FileReferenceCollection $fileReferenceCollection */
-        $fileReferenceCollection = GeneralUtility::makeInstance(FileReferenceCollection::class);
-        return $fileReferenceCollection->getCollectionByPageIdAndField($pageId, 'media');
+        return $this->doktype;
     }
 
-    /**
-     * @param array<string, mixed> $pageToResolve
-     */
-    protected function overwriteFields(array $pageToResolve): void
-    {
-        foreach ($pageToResolve as $key => $pageToResolveValue) {
-            if ($key == 'title' && $pageToResolveValue !== '') {
-                $this->title = $pageToResolveValue;
-            }
-            if ($key == 'media' && $pageToResolveValue !== 0) {
-                $media = self::loadMedia($pageToResolve['uid']);
-                if ($media->count() > 0) {
-                    $this->media = $media;
-                }
-            }
-        }
-    }
-
-    /**
-     * @return string
-     */
     public function getTitle(): string
     {
         return $this->title;
     }
 
-    /**
-     * @return string
-     */
     public function getSubtitle(): string
     {
         return $this->subtitle;
     }
 
-    /**
-     * @return string
-     */
     public function getAbstract(): string
     {
         return $this->abstract;
     }
 
-    /**
-     * @return CategoryCollection
-     */
-    public function getCategories(): CategoryCollection
-    {
-        return $this->categories;
-    }
-
-    /**
-     * @return FileReferenceCollection
-     */
-    public function getMedia(): FileReferenceCollection
-    {
-        return $this->media;
-    }
-
-    /**
-     * @return string
-     */
     public function getJobProfile(): string
     {
         return $this->jobProfile;
     }
 
-    /**
-     * @return string
-     */
     public function getPerformanceScope(): string
     {
         return $this->performanceScope;
     }
 
-    /**
-     * @return string
-     */
     public function getPrerequisites(): string
     {
         return $this->prerequisites;
     }
 
-    /**
-     * @return int
-     */
-    public function getUid(): int
+    public function getAttributes(): ?CategoryCollection
     {
-        return $this->uid;
+        return GeneralUtility::makeInstance(CategoryRepository::class)->findAllByPageId($this->uid);
+    }
+
+    public function getMedia(): ObjectStorage
+    {
+        return $this->media;
     }
 }
