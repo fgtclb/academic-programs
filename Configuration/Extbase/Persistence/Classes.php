7e0e88383a67972d41d8108cc96a45dc16f70d08
Philipp Halle <p.halle@web-vision.de>
2024-10-15 15:06:32 +0200

    [TASK] Map courses model to pages table

---
+++ Configuration/Extbase/Persistence/Classes.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use FGTCLB\EducationalCourse\Domain\Model\Course;
+
+return [
+    Course::class => [
+        'tableName' => 'pages',
+        'properties' => [
+            'doktype' => [
+                'fieldName' => 'doktype',
+            ],
+            'lastUpdated' => [
+                'fieldName' => 'lastUpdated',
+            ],
+            'media' => [
+                'fieldName' => 'media',
+            ],
+            'job_profile' => [
+                'fieldName' => 'job_profile',
+            ],
+            'performanceScope' => [
+                'fieldName' => 'performance_scope',
+            ],
+            'prerequisites' => [
+                'fieldName' => 'prerequisites',
+            ],
+        ],
+    ],
+];
