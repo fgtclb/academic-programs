04506352877e82ce3735c89e3b1b2c66659a3fa0
Philipp Halle <p.halle@web-vision.de>
2024-11-13 16:57:04 +0100

    [TASK] Add breadcrumb label for categories

---
+++ Classes/Backend/Tca/Labels.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FGTCLB\AcademicPrograms\Backend\Tca;
+
+use FGTCLB\AcademicPrograms\Domain\Repository\CategoryRepository;
+use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class Labels
+{
+    /**
+     * @param array<mixed> $params
+     */
+    public function category(&$params): void
+    {
+        $record = BackendUtility::getRecord($params['table'], $params['row']['uid']);
+
+        if ($record !== null) {
+            $categoryRepository = GeneralUtility::makeInstance(CategoryRepository::class);
+            $categoryRootline = $categoryRepository->getCategoryRootline($record['uid']);
+            foreach ($categoryRootline as $category) {
+                $titleParts[] = $category['title'];
+            }
+            $params['title'] = implode(' > ', $titleParts);
+        }
+    }
+}
