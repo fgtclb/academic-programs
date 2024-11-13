b6da0c04cce0ea9d32f2eccf6308e997d5ad0711
Philipp Halle <p.halle@web-vision.de>
2024-11-13 16:30:38 +0100

    [TASK] Remove migrations

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,12 +1,6 @@
 CREATE TABLE pages
 (
-	job_profile       text,
+	job_profile text,
 	performance_scope text,
-	prerequisites     text,
-	shortcut_overwrite tinyint(1) unsigned DEFAULT '0' NOT NULL
-);
-
-
-CREATE TABLE sys_category (
-	tx_migrations_version VARCHAR(14) DEFAULT '' NOT NULL
+	prerequisites text
 );
