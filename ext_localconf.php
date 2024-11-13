a0d569359ba17dd1f0cf5e44c5615a31597e975b
Philipp Halle <p.halle@web-vision.de>
2024-11-13 16:32:02 +0100

    [TASK] Add conditions for configurations depending on TYPO3 version

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,21 +1,22 @@
 <?php
 
 (static function (): void {
-    $studyProgrammeDokType = \FGTCLB\AcademicPrograms\Enumeration\PageTypes::TYPE_EDUCATIONAL_COURSE;
-    // Allow backend users to drag and drop the new page type:
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
-        sprintf(
-            'options.pageTree.doktypesToShowInNewPageDragArea := addToList(%d)',
-            $studyProgrammeDokType
-        )
-    );
-
+    $academicProgramDoktype = \FGTCLB\AcademicPrograms\Enumeration\PageTypes::TYPE_ACADEMIC_PROGRAM;
     $versionInformation = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Information\Typo3Version::class);
-    // Only include page.tsconfig if TYPO3 version is below 12 so that it is not imported twice.
+
     if ($versionInformation->getMajorVersion() < 12) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
+            sprintf(
+                'options.pageTree.doktypesToShowInNewPageDragArea := addToList(%d)',
+                $academicProgramDoktype
+            )
+        );
+
+        // Starting with TYPO3 v12.0 Configuration/page.tsconfig in an Extension is automatically loaded during build time
+        // @see https://docs.typo3.org/m/typo3/reference-tsconfig/12.4/en-us/UsingSetting/PageTSconfig.html#pagesettingdefaultpagetsconfig
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('
-      @import "EXT:academic_programs/Configuration/page.tsconfig"
-   ');
+            @import \'EXT:academic_programs/Configuration/page.tsconfig\'
+        ');
     }
 
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
