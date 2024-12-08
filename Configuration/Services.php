c716d39bbe6bfc5974b649be4008f13dd498403e
Philipp Halle <p.halle@web-vision.de>
2024-12-08 12:45:31 +0100

    [TASK] Add alias for program data processor

--- Configuration/Services.php
+++ Configuration/Services.php
@@ -3,10 +3,15 @@
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use FGTCLB\AcademicPrograms\Collection\FilterCollection;
+use FGTCLB\AcademicPrograms\DataProcessing\ProgramDataProcessor;
 use FGTCLB\AcademicPrograms\Domain\Repository\CategoryRepository;
 use FGTCLB\AcademicPrograms\Domain\Repository\ProgramRepository;
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 return function (ContainerConfigurator $containerConfigurator) {
+    $versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
+
     $services = $containerConfigurator
         ->services()
         ->defaults()
@@ -28,4 +33,17 @@ return function (ContainerConfigurator $containerConfigurator) {
     $services
         ->set(ProgramRepository::class)
         ->public();
+
+    // With TYPO3 version 12.1 an alias can be registered for the data processor
+    if (version_compare($versionInformation->getVersion(), '12.1.0', '>=')) {
+        $services
+            ->set(ProgramDataProcessor::class)
+            ->tag(
+                'data.processor',
+                [
+                    'identifier' => 'program-data',
+                    'label' => 'Program Data Processor',
+                ]
+            );
+    }
 };
