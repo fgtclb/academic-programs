3ad813917d6bc173406f588a8a05cd89a9ab0a87
Philipp Halle <p.halle@web-vision.de>
2024-12-10 14:51:04 +0100

    [TASK] Optimize configuration files

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
-ExtensionManagementUtility::addStaticFile(
-    'academic_programs',
-    'Configuration/TypoScript/',
-    'Academic Programs Page Setup'
-);
+defined('TYPO3') or die;
+
+(static function (): void {
+    ExtensionManagementUtility::addStaticFile(
+        'academic_programs',
+        'Configuration/TypoScript/',
+        'Academic Programs Page Setup'
+    );
+})();
