ee6555d6ec572f81478a1faa78c023b3e2bdecee
Philipp Halle <p.halle@web-vision.de>
2025-02-18 11:42:36 +0100

    [TASK] Remove icon handlig for category types

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -2,23 +2,7 @@
 
 declare(strict_types=1);
 
-use FGTCLB\AcademicPrograms\Enumeration\CategoryTypes;
 use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-
-$iconIdentifier = static fn (string $key): string => 'academic-programs-' . $key;
-
-$iconList = [];
-foreach (CategoryTypes::getConstants() as $type) {
-    $identifier = $iconIdentifier($type);
-    $iconList[$identifier] = [
-        'provider' => SvgIconProvider::class,
-        'source' => sprintf(
-            'EXT:academic_programs/Resources/Public/Icons/CategoryTypes/%s.svg',
-            GeneralUtility::underscoredToUpperCamelCase($type)
-        ),
-    ];
-}
 
 $iconList['academic-programs'] = [
     'provider' => SvgIconProvider::class,
