fbac54ee62c1767e4781880c7c659864d803c91d
Stefan Bürk <stefan@buerk.tech>
2025-04-01 02:36:43 +0200

    [TASK] Replace removed `$GLOBALS['PAGES_TYPES']`
    TYPO3 v12.0 removed `$GLOBALS['PAGES_TYPES']` [1] in
    favour of the new `PageDoktypeRegistry` [2] class.
    
    [1] https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/12.0/Breaking-98487-GLOBALSPAGES_TYPESRemoved.html
    [2] https://docs.typo3.org/m/typo3/reference-coreapi/main/en-us/ApiOverview/PageTypes/CreateNewPageType.html

--- ext_tables.php
+++ ext_tables.php
@@ -1,12 +1,12 @@
 <?php
 
 use FGTCLB\AcademicPrograms\Enumeration\PageTypes;
+use TYPO3\CMS\Core\DataHandling\PageDoktypeRegistry;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 (static function (): void {
-    $studyProgrammeDokType = PageTypes::TYPE_ACADEMIC_PROGRAM;
-
-    $GLOBALS['PAGES_TYPES'][$studyProgrammeDokType] = [
-        'type' => 'web',
-        'allowedTables' => '*',
-    ];
+    GeneralUtility::makeInstance(PageDoktypeRegistry::class)
+        ->add(PageTypes::TYPE_ACADEMIC_PROGRAM, [
+            'allowedTables' => '*',
+        ]);
 })();
