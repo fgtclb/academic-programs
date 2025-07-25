3b556ff705e642d5aacc72578b638b964b572e8d
Stefan Bürk <stefan@buerk.tech>
2025-07-25 11:31:44 +0200

    [RELEASE] 1.2.0
    Used command(s):
    
    ```shell
    clear ; USE_VERSION='1.2.0' ; \
    for packageFolder in packages/fgtclb/*; do \
        composerPackageName="$( jq -r '.name' ${packageFolder}/composer.json )" ; \
        extensionKey="$( jq -r '.extra."typo3/cms"."extension-key"' ${packageFolder}/composer.json )" ; \
        packageNames+=("${composerPackageName}") ; \
        echo "${USE_VERSION}" > ${packageFolder}/VERSION ; \
        tailor set-version ${USE_VERSION} --path ${packageFolder} ; \
    done
    ```

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,7 +5,7 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'Educational Program page for TYPO3 with structured data based on sys_category',
     'category' => 'fe',
     'state' => 'beta',
-    'version' => '1.1.5',
+    'version' => '1.2.0',
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-12.4.99',
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
             'extbase' => '11.5.0-12.4.99',
             'frontend' => '11.5.0-12.4.99',
             'fluid' => '11.5.0-12.4.99',
-            'category_types' => '1.1.5',
+            'category_types' => '1.2.0',
         ],
         'conflicts' => [],
         'suggests' => [
